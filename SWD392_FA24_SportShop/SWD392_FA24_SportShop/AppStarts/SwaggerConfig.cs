c572dcda7e72996007b1040ca1510c78956209b0
Lưu Hiệp SE160641 <luuhiep16092002@gmail.com>
2024-09-06 15:52:00 +0700

    config and setup architecture(50%)

---
+++ SWD392_FA24_SportShop/SWD392_FA24_SportShop/AppStarts/SwaggerConfig.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.OpenApi.Models;
+
+
+namespace SWD392_FA24_SportShop.AppStarts
+{
+    public static class SwaggerConfig
+    {
+        public static void ConfigureSwaggerServices(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                //c.OperationFilter<SnakecasingParameOperationFilter>();
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "FUES API",
+                    Version = "v1"
+                });
+                c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+
+                var securitySchema = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+                c.AddSecurityDefinition("Bearer", securitySchema);
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {securitySchema, new string[] { "Bearer" }}
+                });
+            });
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+    }
+}
---
+++ SWD392_FA24_SportShop/SWD392_FA24_SportShop/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SWD392_FA24_SportShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() 
+        {
+            return Ok("good");
+        }
+    }
+}
